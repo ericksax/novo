749e967b784b9c2397997540f9c563c5f170efd7
ericksax <erickfreitas@gmail.com>
2024-04-23 17:56:27 -0300

    feat: insert new pages notfound and not authorized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
-
+import { isAuthenticated } from './guards/is-authenticated.guard';
+import { isAdmin } from './guards/is-admin.guard';
 export const routes: Routes = [
   {
     path: '',
@@ -15,15 +16,27 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () =>
       import('./pages/home/home.page').then((m) => m.HomePage),
+      canActivate: [isAuthenticated]
   },
   {
     path: 'register',
     loadComponent: () =>
       import('./pages/register/register.page').then((m) => m.RegisterPage),
+      canActivate: [isAuthenticated, isAdmin]
   },
   {
     path: 'document',
     loadComponent: () =>
       import('./pages/document/document.page').then((m) => m.DocumentPage),
+      canActivate: [isAuthenticated, isAdmin]
+  },
+  {
+    path: 'notfound',
+    loadComponent: () => import('./pages/notfound/notfound.page').then( m => m.NotfoundPage)
+  },  {
+    path: 'not-authorized',
+    loadComponent: () => import('./pages/not-authorized/not-authorized.page').then( m => m.NotAuthorizedPage)
   }
+
+
 ];
