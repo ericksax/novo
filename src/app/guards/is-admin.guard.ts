059b24b864a95ed3ac45d779d34919e55696bd07
ericksax <erickfreitas@gmail.com>
2024-04-23 17:57:28 -0300

    feat: inserted guards authenticated and isAdmin

---
+++ src/app/guards/is-admin.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { LoginService } from '../services/login.service';
+
+import { Injectable, inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+class PermissionsService {
+  constructor(private router: Router) { };
+  canActivate(token: string | null ): boolean {
+   const isAdmin = inject(LoginService).isAdmin(token);
+
+    if(!isAdmin) {
+      this.router.navigate(['/not-authorized']);
+      return false
+    }
+
+    return true
+  }
+ }
+
+export const isAdmin: CanActivateFn = (route, state) => {
+  return inject(PermissionsService).canActivate(inject(LoginService).getToken());
+};
---
+++ src/app/guards/is-authenticated.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivateFn } from '@angular/router';
+import { LoginService } from '../services/login.service';
+
+import { Injectable, inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+class PermissionsService {
+  canActivate(token: string | null): boolean {
+   const isExpired =  inject(LoginService).isTokenExpired(token)
+
+    if(isExpired) return false
+    return true
+  }
+ }
+
+export const isAuthenticated: CanActivateFn = (route, state) => {
+  return inject(PermissionsService).canActivate(inject(LoginService).getToken());
+};
