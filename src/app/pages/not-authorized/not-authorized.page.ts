09717fb339613035f300f4d261f431d5162ec91d
ericksax <erickfreitas@gmail.com>
2024-04-24 17:08:55 -0300

    feat: add layout on not-found and unauthorized pages

--- src/app/pages/not-authorized/not-authorized.page.ts
+++ src/app/pages/not-authorized/not-authorized.page.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterLink, RouterLinkActive } from '@angular/router'
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-not-authorized',
   templateUrl: './not-authorized.page.html',
   styleUrls: ['./not-authorized.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButton,
+    RouterLink,
+    RouterLinkActive
+  ]
 })
 export class NotAuthorizedPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    return null
   }
 
 }
